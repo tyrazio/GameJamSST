681b00faf65b7b5b8ee7667b490b66089b6dd76d
LAPTOP-094D1HDD\yuuki80 <tooki2225dy@gmail.com>
2017-11-20 19:13:25 +0900

    出来たあ

---
+++ SST/Assets/tunekawa/Scripts/goal2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goal2 : MonoBehaviour {
+
+    score sc;
+    int p1;
+    int p2;
+    int c1;
+    int c2;
+    void Start()
+    {
+        sc = FindObjectOfType<score>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        p1 = sc.score1;
+        p2 = sc.score2;
+        c1 = sc.count1;
+        c2 = sc.count2;
+    }
+    void OnTriggerEnter(Collider obj)
+    {
+        if (obj.tag == "boal")
+        {
+            if (p1 == 20 && p2 == 20)
+            {
+                sc.Count2();
+            }
+            else
+            {
+                sc.Point2();
+            }
+
+        }
+    }
+}
